0b8aeeb7c9d2ed43270a81431151131188c6a39e
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-07-24 19:02:21 +0100

    change index.html - added links to mobile navigation menu main.js - added mobile-links constant to change the opacity style.css - added styling for mobile navigation links

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const startAnim = document.querySelector("#startAnim");
 const navEl = document.querySelector("#drop-menu");
+const links = document.querySelector(".mobile-links");
 
 startAnim.addEventListener("click", () => {
-    if(navEl.style.height == "30vh"){
-      navEl.style.height = "0";
-      startAnim.children[0].children[0].children[0].className="arrow down"
-
-    }else{
-      navEl.style.height = "30vh";
-      startAnim.children[0].children[0].children[0].className="arrow up"
-
-    }
+	if (navEl.style.height == "30vh") {
+		navEl.style.height = "0";
+		links.style.opacity = 0;
+		startAnim.children[0].children[0].children[0].className = "arrow down";
+	} else {
+		navEl.style.height = "30vh";
+		links.style.opacity = 1;
+		startAnim.children[0].children[0].children[0].className = "arrow up";
+	}
 });
-
