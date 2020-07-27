55ebcc9ea84f908edcba31172221806d13e009a9
Xavier Hazzard <xavier@hazzard.org.uk>
2020-07-27 17:49:20 +0100

    insertion gallery.html change index.html - added link to gallery.html style.css - added responsive styling for gallery link

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const startAnim = document.querySelector("#startAnim");
 const navEl = document.querySelector("#drop-menu");
 const links = document.querySelector(".mobile-links");
+const pictures = document.querySelector("#big-pic");
 
 startAnim.addEventListener("click", () => {
-	if (navEl.style.height == "30vh") {
+	// alert(window.innerHeight);
+
+	if (navEl.style.height == "35vh") {
 		setTimeout(() => {
 			navEl.style.height = "0";
 		}, 130);
 		links.style.opacity = 0;
 		startAnim.children[0].children[0].children[0].className = "arrow down";
 	} else {
-		navEl.style.height = "30vh";
+		navEl.style.height = "35vh";
 		setTimeout(() => {
 			links.style.opacity = 1;
 		}, 150);
 		startAnim.children[0].children[0].children[0].className = "arrow up";
 	}
+});
+
+pictures.addEventListener("click", (event) => {
+	console.log(event.target.src);
 });
