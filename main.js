e7b588ebd3464729ebcb2bea884fe57a7a6b6c8b
Xavier Hazzard <xavier@hazzard.org.uk>
2020-07-28 09:01:30 +0100

    change index.html - added second picture main.js - changed if else to use return

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const startAnim = document.querySelector("#startAnim");
 const navEl = document.querySelector("#drop-menu");
 const links = document.querySelector(".mobile-links");
-const pictures = document.querySelector("#big-pic");
+const pictures = document.querySelector(".big-pic");
+const exit = document.querySelector(".exit");
+const frame = document.querySelector(".pic-full");
+
+var currentImg;
 
 startAnim.addEventListener("click", () => {
 	// alert(window.innerHeight);
@@ -12,15 +16,32 @@ startAnim.addEventListener("click", () => {
 		}, 130);
 		links.style.opacity = 0;
 		startAnim.children[0].children[0].children[0].className = "arrow down";
-	} else {
-		navEl.style.height = "35vh";
-		setTimeout(() => {
-			links.style.opacity = 1;
-		}, 150);
-		startAnim.children[0].children[0].children[0].className = "arrow up";
+		return;
+	}
+	navEl.style.height = "35vh";
+	setTimeout(() => {
+		links.style.opacity = 1;
+	}, 150);
+	startAnim.children[0].children[0].children[0].className = "arrow up";
+});
+
+pictures.addEventListener("click", (e) => {
+	const url = new URL(e.target.src).pathname;
+	const image = document.createElement("img");
+	currentImg = e.target;
+	e.target.style.display = "none";
+	frame.style.display = "block";
+	image.src = url;
+	image.classList.add("full");
+	frame.appendChild(image);
+
+	if (window.innerWidth <= "760px") {
 	}
 });
 
-pictures.addEventListener("click", (event) => {
-	console.log(event.target.getAttribute("src"));
+exit.addEventListener("click", () => {
+	const image = document.querySelector(".full");
+	frame.style.display = "none";
+	frame.removeChild(image);
+	currentImg.style.display = "block";
 });
