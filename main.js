affe28047713c2d76275340c7c9fddfb9822b7a2
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-07-29 13:09:00 +0100

    change completed gallery proto gallery.html main.js style.css

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const startAnim = document.querySelector("#startAnim");
 const navEl = document.querySelector("#drop-menu");
 const links = document.querySelector(".mobile-links");
-
-var currentImg;
+const model = document.querySelector(".model");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption");
+const nav = document.querySelector("nav");
 
 startAnim.addEventListener("click", () => {
 	// alert(window.innerHeight);
@@ -20,4 +23,26 @@ startAnim.addEventListener("click", () => {
 		links.style.opacity = 1;
 	}, 150);
 	startAnim.children[0].children[0].children[0].className = "arrow up";
+});
+
+previews.forEach((preview) => {
+	preview.addEventListener("click", () => {
+		model.classList.add("open");
+		original.classList.add("open");
+		//Dynamically change text and img
+		const originalSrc = preview.getAttribute("data-original");
+		const imgCaption = preview.alt;
+		original.src = `./img/full/${originalSrc}`;
+		imgText.innerText = imgCaption;
+		nav.style.display = "none";
+		
+	});
+});
+
+model.addEventListener("click", (e) => {
+	if (e.target.classList.contains("model")) {
+		model.classList.remove("open");
+		original.classList.remove("open");
+		nav.style.display = "inline-block";
+	}
 });
